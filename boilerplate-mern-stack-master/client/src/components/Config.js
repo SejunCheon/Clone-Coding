765689c286268001b5cbec90ebaf44c3636db399
SejunCheon <kjsug1030@naver.com>
2022-01-03 02:50:50 +0900

    add Title Image

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -3,4 +3,6 @@ export const USER_SERVER = "/api/users";
 
 export const API_URL = "https://api.themoviedb.org/3/";
 
-export const IMAGE_BASE_URL = "https://api.themoviedb.org/t/p/";
+export const API_KEY = "33acdfda0f135caa34d43088b1f195a0";
+
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
