2fd013d9cfb111b3aa0742dbb322ad847d14d7b2
SejunCheon <kjsug1030@naver.com>
2022-01-16 23:55:38 +0900

    add login, token, auth encode

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
-const { User } = require("./models/User");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
+const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올수 있게해줌
 // app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // application/json
 app.use(express.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -33,7 +36,7 @@ app.get("/", (req, res) => {
   res.send("Hello World! 라고할뻔 ㅋ  ㅋ");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원가입 할때 필요한 정보들을 Client에서 가져오면
   // 그것들을 데이터베이스에 넣어준다.
 
@@ -42,6 +45,55 @@ app.post("/register", (req, res) => {
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    // 한 객체만 찾아서 가져온다.
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // 자체제작함수
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      // 비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+// role 1 어드민 role 2 특정 부서 어드민
+// role 0 -> 일반유저 role 0이 아니면 관리자
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastName: req.user.lastName,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt"); // db에 넘어가는 비번을 암호화시키는기능
+const saltRounds = 10; // 10자리를 이용해서 비번 암호화
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -12,7 +15,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxLength: 15,
+    maxLength: 75,
   },
   lastName: {
     type: String,
@@ -32,6 +35,71 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+// mongoose에서 가져온 기능
+userSchema.pre("save", function (next) {
+  var user = this; // userSchema의 user 정보를 가리킨다.
+  // 화살표 함수는 bind기능이 없어서 ES5문법으로 정의했다.
+
+  if (user.isModified("password")) {
+    // password가 변할때 만 암호화해준다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      // 비밀번호를 암호화 시킨다.
+      if (err) return next(err); // err가 발생하면 바로 err를 반환한다.
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+      // 1번째 인자는 순수 DB에들어갈 비밀번호
+      // 2번째 지정해준 salt : 임의의 텍스트
+      // 3번째는 비밀번호를 hash로 변환
+    });
+  } else {
+    next(); // 비밀번호가 아닌 다른것을 할때는 바로 save로 바로 나갈수있게한다.
+  }
+}); // save하기전에 뭔가를 한다는 의미
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword 123456를 암호화해서 암호화된 비밀번호와 비교한다.
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch); // err없음 , isMatch는 true
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this; // 화살표 함수는 bind기능이 없어서 ES5문법으로 정의했다.
+
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  // user._id + 'secretToken' = token 두개를 합쳐서 토큰을 만들고 secretToken이 있으면 user._id를 알수있다.
+  // ->
+  // 'secretToken' -> user._id
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+// statics은 Users 모델에서 바로 쓸 수 있는 메소드
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  // 토큰을 decode한다.
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는 확인
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
